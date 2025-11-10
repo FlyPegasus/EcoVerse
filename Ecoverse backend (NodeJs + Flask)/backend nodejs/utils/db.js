c8a5ca3676cf7144a4b5f8f1cc4b011fbe125638
Anshu Das <anshusignin@gmail.com>
2025-11-10 11:40:16 +0530

    changes from Legion

--- Ecoverse backend (NodeJs + Flask)/backend nodejs/utils/db.js
+++ Ecoverse backend (NodeJs + Flask)/backend nodejs/utils/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  const uri = process.env.MONGO_URI || "mongodb://";
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(uri);
     console.log("mongodb connected successfully.");
   } catch (error) {
     console.log(error);
